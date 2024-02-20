f85ba9fde4211d694619f01cdc4156dfc1e46c95
diiafyra <dii.afyra.work@gmail.com>
2024-02-20 10:01:01 +0700

    tai bt len

---
+++ tổng hợp bt t3 t4/Bài 2/src/bài/pkg2/Bài2.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bài.pkg2;
+
+public class Bài2 {
+
+
+    public static void main(String[] args) {
+        mainFrame mainfr = new mainFrame();
+        mainfr.setVisible(true);
+    }
+    
+}

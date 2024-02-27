02b6f96475de3b8da14173b85fd2af735741d092
diiafyra <dii.afyra.work@gmail.com>
2024-02-28 00:00:09 +0700

    .

---
+++ btt4/btt4.1/src/btt4/pkg1/Btt41.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package btt4.pkg1;
+
+
+public class Btt41 {
+    public static void main(String[] args) {
+        mainFrame mainfr = new mainFrame();
+        mainfr.setVisible(true);
+    }
+}
